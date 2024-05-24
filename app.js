c2f0404b1234cc489352b4b3b6fe76f371c69eb9
fahadarif1 <134074010+fahadarif1@users.noreply.github.com>
2024-05-24 10:20:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function bold(){
+    let area = document.getElementById("area");
+
+    if(area.style.fontWeight == "bold"){
+        area.style.fontWeight = "normal";
+    } 
+    else{
+        area.style.fontWeight = "bold";
+    }
+}
